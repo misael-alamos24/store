f0a454b8deefa0b2df88b590582de78bd45cafd2
Misael <santiagoalamos2000@gmail.com>
2024-10-31 14:38:50 -0300

    store

--- listing/src/App.js
+++ listing/src/App.js
@@ -63,8 +63,8 @@ export default function App () {
 			</div>
 			{!show && button()}
 			{show &&
-			<div className='flex justify-center w100' style={{ position: 'fixed', right: '0px' }}>
-				<div style={{ color: '#fff', backgroundColor: 'rgba(0,0,0,0.5)', borderRadius: '16px', right: ''}} className='text-start padding20 w100'>
+			<div className='flex justify-center w100' style={{ position: 'fixed', right: '40px' }}>
+				<div style={{ color: '#fff', backgroundColor: 'rgba(0,0,0,0.5)', borderRadius: '', right: ''}} className='text-start padding20 w100'>
 					<div id='list' className='w90'> 
 						{/* <span className=''>Lista</span> */}
 						{cart.map((c,i) => <div className='w90 cart-container' key={i}>
